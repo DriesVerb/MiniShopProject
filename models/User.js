8f5f2b982df32c54f116d7fc4622fa9bb9e0eac7
DriesVerb <verboven.dries@gmail.com>
2021-05-03 09:58:27 +0200

    hello

--- models/User.js
+++ models/User.js
@@ -1,21 +1,19 @@
 //create a model Using a Schema
 
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-firstName: String, 
-lastName: String, 
-email: String,
-password: String,
-signUpDate: Date, 
-jobTitle: String, 
-admin: Boolean, 
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  signUpDate: Date,
+  jobTitle: String,
+  admin: Boolean,
+});
 
+//turn Schema into Model
+const User = mongoose.model("User", UserSchema);
 
-}) 
-
-//turn Schema into Model 
-const User = mongoose.model('User', UserSchema)
-
-module.exports=User;
+module.exports = User;
